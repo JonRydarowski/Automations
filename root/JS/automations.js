b06390a7e1407b63b172d629fbf5605a09d8929f
JonRydarowski <jon@bluesombrero.com>
2017-08-04 14:49:25 -0400

    styling

--- root/JS/automations.js
+++ root/JS/automations.js
@@ -1,5 +1,17 @@
 window.onload = function(){
-	
+	 var divs = document.getElementsByTagName("DIV");
+    for (var i = 0; i < divs.length; ++i) {
+        assign(divs[i]);
+    }
+
+    function assign(el) {
+        if ( el.className == "background") {
+            document.addEventListener("scroll", function(){
+                var yPos = -(window.pageYOffset / 6);
+                el.style.backgroundPosition = "30% " + yPos + "px";
+            });
+        }
+    }
 	}
 	
 
