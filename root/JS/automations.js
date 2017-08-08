f01329d1fdc20128f45bccb29cd907a19584ab19
JonRydarowski <jon@bluesombrero.com>
2017-08-08 07:18:06 -0400

    new theme light template

--- root/JS/automations.js
+++ root/JS/automations.js
@@ -1,4 +1,4 @@
-function loadDoc() {
+function lightOne() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -8,3 +8,7 @@ function loadDoc() {
   xhttp.open("GET", "https://maker.ifttt.com/trigger/three/with/key/mYzsy2X2R8crDRxvMX_ygk7AAHd_r4gjE1xks75MrJs", true);
   xhttp.send();
 }
+
+
+
+
